ed616b9ec75a3fb508dfeae7dbcfda75950a72d5
Mingxian Yang <yangmingxian2015@gmail.com>
2023-07-19 18:12:59 +0800

    v1.0.1 Release
    1. 优化了UI以及其他的方面

---
+++ Assets/Scripts/AppUI.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+namespace MX.TextureUnpacker
+{
+    public class AppUI : MonoBehaviour
+    {
+        public GameObject m_Go_BigImageBg;
+        public Image m_Image_BigImage;
+        public TMP_Text m_Text_Tip_New;
+        public Button m_Btn_Excute;
+        public Dropdown m_Dropdown_SelectMode;
+        public Button m_Btn_ContactMe;
+
+        public AppUI Init()
+        {
+            this.m_Go_BigImageBg.gameObject.SetActive(false);
+            this.m_Text_Tip_New.gameObject.SetActive(false);
+
+            this.RegisterEvents();
+            return this;
+        }
+
+        private void RegisterEvents()
+        {
+
+        }
+
+        public void SetTip(string str, bool isError = true)
+        {
+            this.m_Text_Tip_New.gameObject.SetActive(true);
+
+            string prefix = "<color=#FFB1B1>";
+            string postfix = "</color>";
+            if (isError == false)
+            {
+                prefix = "<color=#FFFFFF>";
+            }
+            this.m_Text_Tip_New.text = prefix + str + postfix;
+
+        }
+
+
+        public void SetImage(Texture2D texture)
+        {
+            this.m_Go_BigImageBg.gameObject.SetActive(true);
+            texture.filterMode = FilterMode.Point;
+
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 100f, 0, SpriteMeshType.FullRect);
+
+            this.m_Image_BigImage.sprite = sprite;
+            RectTransform rt = this.m_Image_BigImage.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(texture.width, texture.height);
+
+            //缩放至屏幕可直接显示的大小
+            float minRate = Mathf.Min(642.0f / texture.width, 354.0f / texture.height);
+            if (minRate < 1)
+            {
+                rt.localScale = new Vector2(minRate, minRate);
+            }
+            else
+            {
+                rt.localScale = Vector2.one;
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace MX.TextureUnpacker
+{
+    //入口类，兼职处理App的协程
+    public class Main : MonoBehaviour
+    {
+        void Start()
+        {
+            Application.targetFrameRate = 30;
+            Screen.SetResolution(800, 600, false);
+            new App(this);
+        }
+
+
+        public void OpenUrl(string url)
+        {
+            Application.OpenURL(url);
+        }
+    }
+
+}
